2b8180ab0e8c4dbcfd37de94cfd35c7408578d14
Artfloriani <artfloriani@gmail.com>
2018-07-14 00:57:46 -0300

    fix(style): improve page style

--- custom-types.d.ts
+++ custom-types.d.ts
@@ -1,2 +1,3 @@
 declare module 'react-emoji-render';
 declare module 'react-infinite-scroller';
+declare module 'react-sticky-header';
--- src/components/FilterHeader/FilterHeader.tsx
+++ src/components/FilterHeader/FilterHeader.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import StickyHeader from 'react-sticky-header';
+import 'react-sticky-header/styles.css';
 
 import { FILTER_BUTTONS } from "../../constants";
 import EmojiButton from "../EmojiButton/EmojiButton";
@@ -27,13 +29,17 @@ export default class FilterHeader extends React.Component<
       );
     });
     return (
-      <div className="FilterHeader">
-        <div className="FilterHeader__title">Meew mew meww?</div>
+      <StickyHeader
+      // This is the sticky part of the header.
+      header={
+        <div className="FilterHeader">
+          <div className="FilterHeader__title">Meew mew meww?</div>
 
-        <div className="FilterHeader__filters">
-          { filterButtons }
+          <div className="FilterHeader__filters">
+            { filterButtons }
+          </div>
         </div>
-      </div>
+      } />
     );
   }
 }
