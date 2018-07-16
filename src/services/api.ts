2a242e959580bb3b697563f1183c2d4722fc0358
Artfloriani <artfloriani@gmail.com>
2018-07-16 12:52:59 -0300

    fix(api): use webp instead of gif

--- src/services/api.ts
+++ src/services/api.ts
@@ -27,6 +27,6 @@ export function getGifsFromApi(params: GetGifsParameters) {
 
 const mapGiphyApiResponse = (data: GiphyData[]): GifData[] => {
   return data.map(el => {
-    return { url: el.images.fixed_width.url, id: el.id };
+    return { url: el.images.fixed_width.webp, id: el.id };
   });
 };
